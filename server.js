02f04437e44124a69fec6f8ab2a359ef6d4bef51
Rahmiddin <rahmiddinasiydinov@gmail.com>
2022-02-12 15:39:48 +0500

    cors is added

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 7007;
 const getBook = require('./controllers/getBook');
+const cors = require('cors');
+app.use(cors());
 app.use(express.json());
 
 app.get('/', getBook);
