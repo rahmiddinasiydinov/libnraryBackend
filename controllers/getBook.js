c53baceee6eadd913eb58b61accf79608685b1be
Rahmiddin <rahmiddinasiydinov@gmail.com>
2022-02-13 14:22:30 +0500

    get all is changed

--- controllers/getBook.js
+++ controllers/getBook.js
@@ -6,12 +6,29 @@ const pool  =new Pool({
 
 module.exports = async ( req, res)=>{
     try{
-        const {order, page} = req.body;
+        const {limit, page , sort} = req.query;
+        console.log(page, limit);
+        let numLimit = parseInt(limit);
+        let numPage = parseInt(page);
+        let sortType = sort=='name'?"book_title":sort=='price'?"book_price":"book_author";
         const client = await pool.connect();
-        const {rows:data} =await client.query('SELECT * FROM books ORDER BY book_title OFFSET 0 LIMIT 3;');
+        const {rows:data} =await client.query('SELECT * FROM books ORDER BY $1 OFFSET $2 LIMIT $3', [sortType, (numPage-1)*numLimit, numLimit]);
+
+        
+        const{rows:[{count}]} = await client.query('SELECT COUNT(book_id) FROM books;');
         client.release();
-        console.log(data)
-        res.send(data);
+        let length = Math.ceil(parseInt(count)/numLimit);
+        let arr = []
+        for(let i =0; i<length; i++){
+            arr.push(i+1);
+        }
+        console.log(arr);
+        console.log(data);
+        res.send({
+            data, 
+            pageNum:arr
+
+        });
 
     }catch(e) {
        console.log(e.message);
